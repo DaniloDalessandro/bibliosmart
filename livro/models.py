25ac2396330721a8f486301bdd3b0e9a702900bb
DaniloDalessandro <danilodalessandro08@gmail.com>
2023-07-13 16:41:59 -0300

    Tela de cadastro de usuarios

---
+++ livro/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Livros(models.Model):
+    nome = models.CharField(max_length=100)
+    autor = models.CharField(max_length=30)
+    co_autor = models.CharField(max_length=30,blank=True)
+    data_cadastro = models.DateField(auto_now_add=True)
+    emprestado = models.BooleanField(default=False)
+    nome_emprestado = models.CharField(max_length=30,blank=True)
+    data_emprestimo = models.DateTimeField(blank=True,null=True)
+    data_devolucao = models.DateTimeField(blank=True,null=True)
+    tempo_duracao = models.DateField(verbose_name='Tempo de duração',blank=True,null=True)
+
+    class Meta:
+
+        verbose_name = 'Livro'
+
+    def __str__(self):
+        return self.nome
+        
